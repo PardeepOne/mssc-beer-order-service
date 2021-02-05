07821146c48007ff36ff7dbf3b92b4f4a217e432
Pardeep <pardeeps460@gmail.com>
2021-02-05 23:26:54 +0100

    Implement Validate Order Listener in Beer service

--- src/main/java/guru/sfg/beer/order/service/sm/actions/ValidateOrderAction.java
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/ValidateOrderAction.java
@@ -1,13 +1,11 @@
 package guru.sfg.beer.order.service.sm.actions;
 
-import guru.sfg.beer.brewery.model.BeerOrderDto;
 import guru.sfg.beer.brewery.model.events.ValidateOrderRequest;
 import guru.sfg.beer.order.service.config.JmsConfig;
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
 import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
-import guru.sfg.beer.order.service.services.BeerOrderManager;
 import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
 import guru.sfg.beer.order.service.web.mappers.BeerOrderMapper;
 import lombok.RequiredArgsConstructor;
@@ -38,7 +36,7 @@ public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrde
         String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
         BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
 
-        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE,
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_REQUEST_QUEUE,
                 ValidateOrderRequest.builder().beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder)).build()
         );
 
