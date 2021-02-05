9fa5e1197410d089e6517b7ce0c45be7c463f454
Pardeep <pardeeps460@gmail.com>
2021-02-05 23:26:54 +0100

    Implement Validate Order Listener. closes #23

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -12,7 +12,7 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
-    public static final String VALIDATE_ORDER_QUEUE ="validate-order";
+    public static final String VALIDATE_ORDER_REQUEST_QUEUE ="validate-order-request";
 
     @Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter() {
